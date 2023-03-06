53eab3c45ac025c1ee89d7da06b32ce090df12ad
henriits <121551949+henriits@users.noreply.github.com>
2023-03-06 19:50:42 +0200

    bill form class, and results page class, with post and get methods

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
 from flask.views import MethodView
-from wtforms import Form
-from flask import Flask
+from wtforms import Form, StringField, SubmitField
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 class HomePage(MethodView):
     def get(self):
-        return "Hello"
+        return render_template("index.html")
 
 
 class BillFormPage(MethodView):
     def get(self):
-        return "Hello"
+        bill_form = BillForm()
+        return render_template("bill_form_page.html", bill_form=bill_form)
 
 
 class ResultPage(MethodView):
-    pass
+
+    def post(self):
+        billform = BillForm(request.form)
+        amount = billform.amount.data
+        return render_template("results.html")
+
 
 
 class BillForm(Form):
-    pass
+
+    amount = StringField("Bill Amount: ")
+    period = StringField("Bill Period: ")
+
+    name1 = StringField("Name: ")
+    days_in_house1 = StringField("Days in house: ")
+
+    name2 = StringField("Name: ")
+    days_in_house2 = StringField("Days in house: ")
+
+    button = SubmitField("Calculate")
+
 
 
 app.add_url_rule("/", view_func=HomePage.as_view("home_page"))
-app.add_url_rule("/bill", view_func=BillFormPage.as_view("bill_form_page"))
+app.add_url_rule("/bill_form", view_func=BillFormPage.as_view("bill_form_page"))
+app.add_url_rule("/results", view_func=ResultPage.as_view("results_page"))
 
-app.run()
+app.run(debug=True)

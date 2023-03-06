f0b71df92f5d81b98f4e368f6dfe1b3bf2bfab5d
henriits <121551949+henriits@users.noreply.github.com>
2023-03-06 18:56:13 +0200

    Adding Flask

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from flask.views import MethodView
 from wtforms import Form
+from flask import Flask
+
+app = Flask(__name__)
 
 
 class HomePage(MethodView):
-    pass
+    def get(self):
+        return "Hello"
 
 
 class BillFormPage(MethodView):
-    pass
+    def get(self):
+        return "Hello"
 
 
 class ResultPage(MethodView):
@@ -16,3 +21,9 @@ class ResultPage(MethodView):
 
 class BillForm(Form):
     pass
+
+
+app.add_url_rule("/", view_func=HomePage.as_view("home_page"))
+app.add_url_rule("/bill", view_func=BillFormPage.as_view("bill_form_page"))
+
+app.run()
